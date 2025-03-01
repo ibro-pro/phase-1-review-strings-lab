d6ded940b242b4b522ee5fdf85d7282f0082cec9
root <root@DESKTOP-IFL1C2R.>
2025-03-01 17:14:57 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Ibrahim';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(0);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
